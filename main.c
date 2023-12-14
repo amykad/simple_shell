135e3c8e566b7712351a26ea5ef9d13c7449aa97
amykad <amina.kaddal@gmail.com>
2023-12-14 08:38:38 -0800

    main.c

--- main.c
+++ main.c
@@ -1,51 +1,72 @@
 #include "shell.h"
+
 /**
- * main - Main arguments functions
- * @ac: Count of argumnents
- * @av: Arguments
- * @env: Environment
- * Return: _exit = 0.
+ * print_prompt - Prints the shell prompt if
+ * the standard input is a terminal.
  */
-int main(int ac, char **av, char **env)
-{
-int pathValue = 0, status = 0, is_path = 0;
-char *line = NULL, /**ptr to inpt*/ **commands = NULL; /**tokenized commands*/
-(void)ac;
-while (1)/* loop until exit */
-{
-errno = 0;
-line = _getline_command();/** reads user input*/
-if (line == NULL && errno == 0)
-return (0);
-if (line)
-{
-pathValue++;
-commands = tokenize(line);/** tokenizes or parse user input*/
-if (!commands)
-free(line);
-if (!_strcmp(commands[0], "env"))/**checks if user wrote env"*/
-_getenv(env);
-else
-{
-is_path = _values_path(&commands[0], env);/** tokenizes PATH*/
-status = _fork_fun(commands, av, env, line, pathValue, is_path);
-if (status == 200)
-{
-free(line);
-return (0);
-}
-if (is_path == 0)
-free(commands[0]);
-}
-free(commands); /*free up memory*/
-}
-else
+void print_prompt(void)
 {
 if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "\n", 1);/** Writes to standard output*/
-exit(status);
+{
+printf("$ ");
+fflush(stdout);
 }
+}
+
+/**
+ * cleanup - Frees allocated memory for command execution.
+ * @line: The input line.
+ * @alternative_command: The tokenized alternative command.
+ * @original_path: The original PATH environment variable.
+ */
+void cleanup(char *line, char **alternative_command, char *original_path)
+{
 free(line);
+free(original_path);
+for (int i = 0; alternative_command[i] != NULL; i++)
+{
+free(alternative_command[i]);
 }
-return (status);
+free(alternative_command);
+}
+
+/**
+ * main - The main function for the shell program.
+ *
+ * Return: Returns 0 on successful execution.
+ */
+int main(void)
+{
+char *line = NULL, **alternative_command = NULL;
+char *path = getenv("PATH");
+int alternative_status = 0;
+char *original_path = strdup(path);
+
+while (1)
+{
+pid_t child_pid;
+
+print_prompt();
+ine = custom_read_line();
+
+if (line == NULL)
+{
+if (isatty(STDIN_FILENO))
+{
+printf("\n");
+}
+cleanup(line, alternative_command, original_path);
+return (alternative_status);
+}
+
+alternative_command = tokenize(line);
+if (!alternative_command)
+{
+cleanup(line, alternative_command, original_path);
+continue;
+}
+
+cleanup(line, alternative_command, original_path);
+}
+return (0);
 }
